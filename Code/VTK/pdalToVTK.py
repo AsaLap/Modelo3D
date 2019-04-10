6081c47a88d3c3d01d751668cb5e823b8ad91ef5
Clairecita <48314825+Clairecita@users.noreply.github.com>
2019-04-10 13:48:23 +0200

    triangulation + obj + timer
    avec le timer pour les tests

--- Code/VTK/pdalToVTK.py
+++ Code/VTK/pdalToVTK.py
@@ -6,9 +6,11 @@
 
 from vtk import *
 import csv
-
+import time
+ 
 points = vtkPoints()
 
+beginning = time.time()
 
 with open('essai.csv', 'r') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
@@ -16,13 +18,14 @@ with open('essai.csv', 'r') as csvfile:
     i = 0
     j=0
     for row in spamreader:
-        if (i != 0 and i%50==0) : #pour ne pas prendre en compte le header
+        if (i != 0) : #pour ne pas prendre en compte le header
             #if (row[8]== '2.000') : #seulement pour les fichiers issus d'un lidar
                 points.InsertNextPoint(int(float(row[0])),int(float(row[1])), int(float(row[2])))
                 j=j+1
         i = i+1
 print ("nombre de points importés  : ",j)
-
+print(time.time() - beginning)
+beginning = time.time()
 #triangulation
 profile = vtkPolyData()
 profile.SetPoints(points)
@@ -30,11 +33,16 @@ profile.SetPoints(points)
 delny = vtkDelaunay2D()
 delny.SetInputData(profile)
 delny.SetTolerance(0.01)
+
 mapMesh = vtkPolyDataMapper()
 mapMesh.SetInputConnection(delny.GetOutputPort())
 meshActor = vtkActor()
 meshActor.SetMapper(mapMesh)
 meshActor.GetProperty().SetColor(.1, .2, .4)
+print("triangulation")
+print(time.time() - beginning)
+beginning = time.time()
+
 
 #rendering
 ren = vtkRenderer()
@@ -49,12 +57,18 @@ ren.SetBackground(1, 1, 1)
 renWin.SetSize(250, 250)
 renWin.Render()
 
+print("rendering")
+print(time.time() - beginning)
+beginning = time.time()
+
 #export au format OBJ
 obj = vtkOBJExporter()
 obj.SetFilePrefix("essai")
 obj.SetRenderWindow(renWin)
 obj.Write()
-print ("fichier obj écrit")
+
+print("écriture fichier")
+print(time.time() - beginning)
 
 cam1 = ren.GetActiveCamera()
 cam1.Zoom(1.5)
