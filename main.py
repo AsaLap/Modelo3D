6382a96108b405ca85e95368c4fed60a0a4c4df5
AsaLap <asalaporte@gmail.com>
2019-04-23 16:18:38 +0200

    Modification du menu

--- main.py
+++ main.py
@@ -5,42 +5,35 @@ import BDDconnexion
 # import OBJtoVTK
 import PDALtoVTK
 
-def menu():
-    GoOn = True
-    while (GoOn):
-        choix = [
-            'Ajouter un fichier source pour le traiter',
-            'Ajouter un fichier source pour l\'enregistrer dans le base de données',
-            'Effectuer un traitement sur un fichier existant sur la base de données',
-            'Visualiser un maillage pré-traité',
-            'Récupérer un fichier au format OBJ (post-traitement)',
-            'Faire une requête libre sur la base de données (dev)',
-            'Quitter'
-            ]
-        for i in range(len(choix)):
-            print(str(i+1) + " : " + choix[i])
-        choix = int(input())
-        if (choix == 1):
-            addFileToRun()
-        elif (choix == 2):
-            addFileToStore()
-        elif (choix == 3):
-            runProcess()
-        elif (choix == 4):
-            view()
-        elif (choix == 5):
-            getOBJ()
-        elif (choix == 6):
-            requete()
-        elif (choix == 7):
-            GoOn = False
-        else:
-            print("Demande non comprise")
-
 def requete():
     query = str(input("Veuillez entrer une requête SQL (SELECT...FROM...WHERE...) : "))
     res = BDDconnexion.connexion(query)
     return res
 
+def quit():
+    global GoOn
+    GoOn = False
+
+def menu():
+    while (GoOn):
+        choix = [
+            ['Ajouter un fichier source pour le traiter',lambda : addFileToRun()],
+            ['Ajouter un fichier source pour l\'enregistrer dans le base de données',lambda : addFileToStore()],
+            ['Effectuer un traitement sur un fichier existant sur la base de données',lambda : runProcess()],
+            ['Visualiser un maillage pré-traité',lambda : view()],
+            ['Récupérer un fichier au format OBJ (post-traitement)',lambda : getOBJ()],
+            ['Faire une requête libre sur la base de données (developpeurs uniquement)',lambda : requete()],
+            ['Quitter', lambda : quit()]
+            ]
+        for i in range(len(choix)):
+            print(str(i+1) + " : " + choix[i][0])
+        numChoix = int(input())
+        try:
+            choix[numChoix-1][1]() #Appel de lafonction contenue dans la liste à l'indice donné par l'utilisateur
+        except:
+            print("Echec de la fonction")
+
+
 if __name__=='__main__':
+    GoOn = True
     menu()
